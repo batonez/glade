566fc05228e4adc58235182213d7d10c53df381e
batonez <batonez@gmail.com>
2015-09-20 21:32:18 +0300

    Common interface for font classes

---
+++ include/glade/ui/font/Font.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glade/GladeObject.h>
+
+class Font
+{
+  protected:
+    std::shared_ptr<ShaderProgram> shaderProgram;
+    float stringScaleX, stringScaleY;
+    
+  public:
+    Font(): stringScaleX(0), stringScaleY(0) {}
+  
+    virtual void setFontSizePixels(unsigned int width, unsigned int height = 0) = 0;
+    virtual GladeObject::Drawables *createDrawablesForString(const std::string &string) = 0;
+    
+    void setShaderProgram(std::shared_ptr<ShaderProgram> &shader_program)
+    {
+      shaderProgram = shader_program;
+    }
+    
+    float getLastStringScaleX() { return stringScaleX; }
+    float getLastStringScaleY() { return stringScaleY; }
+};

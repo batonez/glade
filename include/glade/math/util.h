2718bff78f945e6fcfa77f2cff1bcfe463528a6c
batonez <batonez@gmail.com>
2015-09-20 21:37:26 +0300

    Added throw_coin function to math module

--- include/glade/math/util.h
+++ include/glade/math/util.h
@@ -1,1 +1,6 @@
+#pragma once
+
 extern double DOUBLE_CUBE_DIAGONAL;
+
+// parameter is the probability of returning true
+bool throw_coin(float probability);
--- src/math/globals.cpp
+++ src/math/globals.cpp
@@ -1,3 +1,9 @@
 #include <math.h>
+#include <stdlib.h>
 
 double DOUBLE_CUBE_DIAGONAL = 1.4422495703074083823216383107801;
+
+bool throw_coin(float probability)
+{
+  return (::rand() % 100) < ::round(probability * 100);
+}

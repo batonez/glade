79bd3a84b48fa400bfd835a7ec7778597f42d099
batonez <batonez@gmail.com>
2015-09-20 21:31:47 +0300

    Changes for compilind with Android NDK (2)

--- include/glade/math/Transform.h
+++ include/glade/math/Transform.h
@@ -15,7 +15,8 @@ private:
 public:
 	static float constrainAngle(float angle) { return angle < 360.0f ? angle : fmod(angle, 360.0f); }
 	
-  Transform(void)
+  Transform(void):
+    invertor(1)
   {
     position.set(0, 0, 0);
     rotation.set(0, 0, 0);
@@ -23,7 +24,7 @@ public:
   }
 	
 	Transform(float posX, float posY, float posZ, float rotX, float rotY, float rotZ, float scaleX, float scaleY, float scaleZ) :
-  invertor(1)
+    invertor(1)
   {
 		position.set(posX, posY, posZ);
 		rotation.set(rotX, rotY, rotZ);

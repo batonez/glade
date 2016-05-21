aff60d8747aeb9a9ab2abc1672b4112887c51556
a.reshetnikov <a.reshetnikov@gaijin.ru>
2016-05-22 01:11:48 +0300

    Added degrees_to_radians() function

--- include/glade/math/util.h
+++ include/glade/math/util.h
@@ -1,6 +1,8 @@
 #pragma once
 
 extern double DOUBLE_CUBE_DIAGONAL;
+extern float  RADIANS_IN_DEGREE;
 
 // parameter is the probability of returning true
 bool throw_coin(float probability);
+float degrees_to_radians(float degrees);
--- src/math/globals.cpp
+++ src/math/globals.cpp
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 
 double DOUBLE_CUBE_DIAGONAL = 1.4422495703074083823216383107801;
+float  RADIANS_IN_DEGREE = 0.0174533f;
 
 bool throw_coin(float probability)
 {
   return (::rand() % 100) < ::round(probability * 100);
+}
+
+float degrees_to_radians(float degrees)
+{
+  return degrees * RADIANS_IN_DEGREE;
 }
